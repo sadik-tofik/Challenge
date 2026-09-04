f3a8e05c1ba0613a0f38a1f803f1c4a0df952867
sadik-tofik <sadiktofik168@gmail.com>
2026-09-04 18:38:41 +0300

    Simplify ping response handling

--- src/PingerService.php
+++ src/PingerService.php
@@ -10,15 +10,10 @@ use Spiral\GRPC\ContextInterface;
 class PingerService implements PingerInterface
 {
     public function Ping(ContextInterface $ctx, PingRequest $in): PingResponse
-    {
-        // 1. Read message from $in using getMessage()
-        $Message = $in->getMessage();
-    
-        // 2. Create a new PingResponse object
-        $response = new PingResponse();
-        // 3. Set the message on the response object using setMessage()
-        $response->setMessage($Message);
-        // 4. Return the response object
-        return $response;
-    }
+{
+    $response = new PingResponse();
+    $response->setMessage($in->getMessage());
+
+    return $response;
+}
 }
